ac0e407ad22c1d413fde8551b113251a16f305db
Peter Vandenabeele <peter@vandenabeele.com>
2012-01-11 18:39:39 +0100

    seconds_since_midnight should_not raise_error

--- lib/relativity/relative_time.rb
+++ lib/relativity/relative_time.rb
@@ -1,2 +1,5 @@
 class RelativeTime
+  def seconds_since_midnight
+    0
+  end
 end
--- spec/relative_time_spec.rb
+++ spec/relative_time_spec.rb
@@ -1,9 +1,16 @@
 require 'spec_helper'
 
 describe RelativeTime do
+
+  it "new creates a RelativeTime" do
+    lambda { RelativeTime.new }.should_not raise_error
+  end
+
   context "time in a day" do
-    it "new creates a RelativeTime" do
-      lambda { RelativeTime.new }.should_not raise_error
+
+    it "seconds_since_midnight" do
+      lambda { subject.seconds_since_midnight }.should_not raise_error
     end
+
   end
 end
