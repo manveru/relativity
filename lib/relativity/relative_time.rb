a229aee96bfa1e5abbc759cac328ac364e1f7196
Peter Vandenabeele <peter@vandenabeele.com>
2012-01-11 19:07:03 +0100

    RelativeTime.new creates a RelativeTime close to now

--- lib/relativity/relative_time.rb
+++ lib/relativity/relative_time.rb
@@ -1,5 +1,12 @@
 class RelativeTime
-  def seconds_since_midnight
-    0
+
+  attr_reader :seconds_since_midnight
+
+  def initialize
+    super()
+    t = Time.new
+    t_s = (t.hour * 60 + t.min) * 60 + t.sec
+    @seconds_since_midnight = t_s
   end
+
 end
--- spec/relative_time_spec.rb
+++ spec/relative_time_spec.rb
@@ -3,13 +3,20 @@ require 'spec_helper'
 describe RelativeTime do
 
   it "new creates a RelativeTime" do
-    lambda { RelativeTime.new }.should_not raise_error
+    lambda { subject }.should_not raise_error
   end
 
   context "time in a day" do
 
     it "seconds_since_midnight" do
       lambda { subject.seconds_since_midnight }.should_not raise_error
+    end
+
+    it "new creates a RelativeTime close to now" do
+      rt_s = subject.seconds_since_midnight
+      t = Time.new
+      t_s = (t.hour*60 + t.min)*60 + t.sec # seconds_since_midnight from Time
+      ((t_s-rt_s)%(24*60*60)).abs.should be < 10
     end
 
   end
